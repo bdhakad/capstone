da32b52f487a98fbbc6b67c60a830baf3f36505c
bdhakad <brajeshdhakad50@gmail.com>
2021-10-15 19:24:20 +0530

    test deployment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World! - Green'))
+app.get('/', (req, res) => res.send('Hello World! - RED'))
 app.listen(80, () => console.log('Server ready'))
