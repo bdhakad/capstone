cae5e1e94763e0b71aec020b998e1e39d34ca2e6
bdhakad <brajeshdhakad50@gmail.com>
2021-10-15 22:01:08 +0530

    lint failing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-
+let k;
 app.get('/', (req, res) => res.send('Hello World! - Brajesh Dhakad'))
 app.listen(80, () => console.log('Server ready'))
