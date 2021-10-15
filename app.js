1719ad497eefe3c36a5680b3bb1626877b4da461
bdhakad <brajeshdhakad50@gmail.com>
2021-10-15 18:29:50 +0530

    build docker image

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! - 1.0.0'))
 app.listen(80, () => console.log('Server ready'))
